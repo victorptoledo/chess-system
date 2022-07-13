2c3cd84a224ec12f3789ea7ebd80ff42e7892807
Victor Toledo <victorptoledo@outlook.com>
2022-07-13 19:34:39 -0300

    Primeira classe: Position

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Position position = new Position(2,4);
+
+        System.out.println(position);
+    }
+}
